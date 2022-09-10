9508786b3d3f3d176e5a34f32719239df843dde8
junseokoh-hub <lunulatecat@naver.com>
2022-09-10 18:29:25 +0900

    redux advance base

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "./counterSlice";
-import authSlice from "./authSlice";
-
-const store = configureStore({
-  reducer: { counter: counterSlice, auth: authSlice },
-});
-
-export default store;
